9d77da22e3b7dc3deebe9cb8a939f4762956ab90
jackf21 <jack.fenning04@gmail.com>
2024-05-14 16:58:03 +0100

    Basic movement

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,1 +1,15 @@
 extends CharacterBody2D
+
+const SPEED := 300
+
+func _physics_process(delta):
+	var x_direction = Input.get_axis("left", "right")
+	var y_direction = Input.get_axis("up", "down")
+	
+	if x_direction or y_direction:
+		velocity = Vector2(x_direction * SPEED, y_direction * SPEED)
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+	
+	move_and_slide()
