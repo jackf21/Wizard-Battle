1aa91189fc58c6b7064269af3adbdb9967292be2
jackf21 <jack.fenning04@gmail.com>
2024-05-17 12:18:29 +0100

    Added a player class  and changed some file names

--- Scripts/player_character.gd
+++ Scripts/player_character.gd
@@ -1,21 +1,14 @@
+class_name Player
+
 extends CharacterBody2D
 
 const SPEED := 400
-const BASIC_PROJECTILE = preload("res://Scenes/basic_projectile.tscn")
 
 var shoot_ready: bool = true
+@export var projectile_scene: PackedScene
 @onready var shoot_cooldown = $ShootCooldown
 @onready var face = $Face
 
-func _input(event):
-	if event.is_action_pressed("shoot_primary") and shoot_ready:
-		shoot_ready = false
-		var basic_projectile = BASIC_PROJECTILE.instantiate()
-		get_parent().add_child(basic_projectile)
-		basic_projectile.position = face.global_position
-		basic_projectile.rotation = rotation
-		shoot_cooldown.start()
-		
 func _physics_process(_delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * SPEED
@@ -24,6 +17,15 @@ func _physics_process(_delta):
 
 func _process(_delta):
 	look_at(get_global_mouse_position())
+
+func _input(event):
+	if event.is_action_pressed("shoot_primary") and shoot_ready:
+		shoot_ready = false
+		var basic_projectile = projectile_scene.instantiate()
+		get_parent().add_child(basic_projectile)
+		basic_projectile.position = face.global_position
+		basic_projectile.rotation = rotation
+		shoot_cooldown.start()
 
 func _on_shoot_cooldown_timeout():
 	shoot_ready = true
