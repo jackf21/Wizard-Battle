56657269e980777385d1d72b2f83243f8d3aa70d
jackf21 <jack.fenning04@gmail.com>
2024-05-19 12:23:46 +0100

    Added damage system with fire causind damage over time and ice causing a slowing effect

---
+++ Scripts/game_entity.gd
@@ -0,0 +1,59 @@
+class_name Entity
+
+extends CharacterBody2D
+
+enum damage_type{ UNTYPED, FIRE, ICE }
+
+@export var speed: float
+@export var max_health: float
+var current_health: float
+var counter := 0
+var timers = {}
+
+func _ready():
+	current_health = max_health
+	damage_entity(30, damage_type.ICE)
+
+func damage_entity(damage_value: float, type: damage_type):
+	match type:
+		damage_type.UNTYPED:
+			current_health -= damage_value
+		damage_type.FIRE:
+			current_health -= damage_value
+			apply_dot_effect( 0.5, 10, damage_value * 0.1)
+		damage_type.ICE:
+			current_health -= damage_value
+			apply_slow_effect(damage_value * 0.1, 0.8)
+
+func apply_dot_effect(duration_between_damage : float, repeat_time : int, damage : float):
+	var dot_timer = Timer.new()
+	dot_timer.wait_time = duration_between_damage
+	dot_timer.autostart = true
+	dot_timer.timeout.connect(burn.bind(damage))
+	dot_timer.timeout.connect(time_out_timer_damage.bind(counter))
+	timers[counter] = {repeat = repeat_time, timer = dot_timer, damage = damage}
+	add_child(dot_timer)
+	counter += 1
+
+func apply_slow_effect(duration_of_slow: float, slow_value: float):
+	speed *= slow_value
+	var slow_timer = Timer.new()
+	slow_timer.wait_time = duration_of_slow
+	slow_timer.autostart = true
+	slow_timer.timeout.connect(remove_slow.bind(slow_value))
+	slow_timer.timeout.connect(time_out_timer_damage.bind(counter))
+	timers[counter] = {repeat = 1, timer = slow_timer, damage = 0}
+	add_child(slow_timer)
+	counter += 1
+
+func time_out_timer_damage(id):
+	timers[id].repeat -= 1
+	if (timers[id].repeat == 0):
+		timers[id].timer.call_deferred("queue_free")
+		timers.erase(id)
+
+func burn(damage):
+	current_health -= damage
+
+func remove_slow(slow_value):
+	speed *= 1/slow_value
--- Scripts/player_character.gd
+++ Scripts/player_character.gd
@@ -1,23 +1,22 @@
 class_name Player
 
-extends CharacterBody2D
+extends Entity
 
-const SPEED := 400
-
-var health: int
 var shoot_ready: bool = true
 @export var projectile_scene: PackedScene
+@export var health_label: Label
 @onready var shoot_cooldown = $ShootCooldown
 @onready var face = $Face
 
 func _physics_process(_delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
-	velocity = direction * SPEED
+	velocity = direction * speed
 	velocity.normalized()
 	move_and_slide()
 
 func _process(_delta):
 	look_at(get_global_mouse_position())
+	health_label.text = str(current_health)
 
 func _input(event):
 	if event.is_action_pressed("shoot_primary") and shoot_ready:
@@ -31,5 +30,3 @@ func _input(event):
 func _on_shoot_cooldown_timeout():
 	shoot_ready = true
 
-func player_damage(damage_value: int):
-	health -= damage_value
