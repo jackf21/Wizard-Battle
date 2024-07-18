bafdd4c36f4fda94ddfc146f77480ff82291e748
jackf21 <jack.fenning04@gmail.com>
2024-07-18 11:25:24 +0100

    Finding the distance between the player nad the entity

--- Scripts/shooter_hostile_01.gd
+++ Scripts/shooter_hostile_01.gd
@@ -1,3 +1,14 @@
 class_name Shooter_hostile
 
 extends Entity
+
+@onready var player = $"../player"
+
+#func _ready():
+#	print(player.transform)
+
+func _process(delta):
+	var player_position = player.transform
+	var entity_position = transform
+	
+	var dist_to_player = entity_position.x - player_position.x
