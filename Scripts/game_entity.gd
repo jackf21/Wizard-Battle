be7233f542646079528527aba8c695a13c67b2a7
jackf21 <jack.fenning04@gmail.com>
2024-07-31 13:20:06 +0100

    Added comments to entity script

--- Scripts/game_entity.gd
+++ Scripts/game_entity.gd
@@ -30,18 +30,31 @@ func damage_entity(damage_value: float, type: damage_types):
 			if(!is_ice_slowed):
 				apply_slow_effect(damage_value * 0.1, 0.8)
 
-func apply_dot_effect(duration_between_damage : float, repeat_time : int, damage : float):
+func apply_dot_effect(duration_between_damage : float, num_repeats : int, damage : float):
+	
+	# Boolean to prevent multiple stacks of a dot effect being attached
 	is_burning = true
+	
+	# Adding a new timer to count down between each tick of damage 
 	var dot_timer = Timer.new()
 	dot_timer.wait_time = duration_between_damage
 	dot_timer.autostart = true
+	
+	# The functions for damaging the player and counting down to removing the timer are attached and ran when the timer ends
 	dot_timer.timeout.connect(burn.bind(damage))
 	dot_timer.timeout.connect(time_out_timer_damage.bind(counter))
-	timers[counter] = {repeat = repeat_time, timer = dot_timer, damage = damage, timer_type = "dot"}
+	
+	# Adds the timer to the timer array and adds it as a child of the entity
+	timers[counter] = {repeat = num_repeats, timer = dot_timer, damage = damage, timer_type = "dot"}
 	add_child(dot_timer)
+	
+	# Incrementing counter variable for array access
 	counter += 1
 
 func apply_slow_effect(duration_of_slow: float, slow_value: float):
+	#
+	# Works similarly to the dot effect but the timer only repeats once
+	#
 	is_ice_slowed = true
 	speed *= slow_value
 	var slow_timer = Timer.new()
@@ -54,16 +67,24 @@ func apply_slow_effect(duration_of_slow: float, slow_value: float):
 	counter += 1
 
 func time_out_timer_damage(id):
-	timers[id].repeat -= 1
+	
+	# Counting down to remove the timer 
+	timers[id].repeat -= 1 
 	if (timers[id].repeat == 0):
+		
+		# allowing for the player to be burned again only if the timer has finished repeating
 		if (timers[id].timer_type == "dot"):
 			is_burning = false
+		
+		# Deletes the timer from the scene and the array
 		timers[id].timer.call_deferred("queue_free")
 		timers.erase(id)
 
+# Function for damaging the players health when the dot timer runs out
 func burn(damage):
 	current_health -= damage
 
+# Restores the players speed back to normal and alllows them to be slowed again
 func remove_slow(slow_value):
 	speed *= 1/slow_value
 	is_ice_slowed = false
